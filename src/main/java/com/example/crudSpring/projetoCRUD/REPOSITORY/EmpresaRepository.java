303ddcef90031cb2541165b4516bf54ac6ed5c07
Guilherme <guilhermeda2008@gmail.com>
2025-09-15 20:39:38 -0300

    (15-09-25)

--- src/main/java/com/example/crudSpring/projetoCRUD/REPOSITORY/EmpresaRepository.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/REPOSITORY/EmpresaRepository.java
@@ -1,11 +1,14 @@
 package com.example.crudSpring.projetoCRUD.REPOSITORY;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
 
 @Repository
-public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
-    
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    List<Empresa> findByNome_empresaContainingIgnoreCase(String nome_empresa);
 }
--- src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
@@ -1,6 +1,7 @@
 package com.example.crudSpring.projetoCRUD.SERVICE;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -12,44 +13,50 @@ public class EmpresaService {
 
     private final EmpresaRepository empresaRepository;
 
-    //metodo construtor da classe EmpresaService
-    //criando uma ligação cp, a Classe EmpresaRepository
+    // metodo construtor da classe EmpresaService
+    // criando uma ligação cp, a Classe EmpresaRepository
 
-    public EmpresaService (EmpresaRepository ligacaoEmpresaRepository) {
+    public EmpresaService(EmpresaRepository ligacaoEmpresaRepository) {
         empresaRepository = ligacaoEmpresaRepository;
     }
 
     public List<Empresa> findAll() {
         return empresaRepository.findAll();
-    }//select * from empresa
-    //seleciona todas as empresas do banco de dados
-     //em formato de lista
-     
-     
-     //empresaRepository: É uma referência a uma instância da classe EmpresaRepository. Esta instância é injetada automaticamente pelo Spring, geralmente no construtor da classe de serviço
+    }// select * from empresa
+     // seleciona todas as empresas do banco de dados
+     // em formato de lista
 
-    //      Object                          //nome do parametro (formulario dentro da controller)
-     public Empresa cadastrarEmpresa(Empresa dadosEmpresa){
+    // empresaRepository: É uma referência a uma instância da classe
+    // EmpresaRepository. Esta instância é injetada automaticamente pelo Spring,
+    // geralmente no construtor da classe de serviço
+
+    // Object //nome do parametro (formulario dentro da controller)
+    public Empresa cadastrarEmpresa(Empresa dadosEmpresa) {
         return empresaRepository.save(dadosEmpresa);
-     }//metodo cadastrarEmpresa
+    }// metodo cadastrarEmpresa
 
-     // sem retorno     para ver retorno executar novo listar  -> frontend
-     public void deletarEmpresa(Empresa dadosEmepresa){
+    // sem retorno para ver retorno executar novo listar -> frontend
+    public void deletarEmpresa(Empresa dadosEmepresa) {
         empresaRepository.delete(dadosEmepresa);
-     }//metodo deletarEmpresa
+    }// metodo deletarEmpresa
 
-     }
+    public Optional<Empresa> buscarPorId(Long id) {
+        return empresaRepository.findById(id);
 
+    }
 
+    public Empresa editarDadoEmpresa(Long id, Empresa dadosAtualizados) {
+        return null;
+    }
+}
 
-
-//BANCO
-//SELECTS OU INSERTS OU ALTER TABLE OU DELETE
-//SELECT * FROM EMPRESA (SELECT REALIZA LISTAGEM DE DADOS)
+// BANCO
+// SELECTS OU INSERTS OU ALTER TABLE OU DELETE
+// SELECT * FROM EMPRESA (SELECT REALIZA LISTAGEM DE DADOS)
 // -> RETORNA TODOS OS DADOS DA EMPRESA (* == TUDO DA TABELA)
 
-//  1°                     2°                                FROM  3°                                      
-// SELECT (QUAIS OS DADOS QUE EU QUERO RETORNAR?) (ONDE DESEJA EXECUTAR O COMANDO?)
+// 1° 2° FROM 3°
+// SELECT (QUAIS OS DADOS QUE EU QUERO RETORNAR?) (ONDE DESEJA EXECUTAR O
+// COMANDO?)
 
-
-//WHERE nome_empresa = 'Nome da Empresa'; ????????????????????
+// WHERE nome_empresa = 'Nome da Empresa'; ????????????????????
