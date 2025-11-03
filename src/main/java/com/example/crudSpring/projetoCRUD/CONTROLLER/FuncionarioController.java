d41fe029ed315f6ef853e2afbf2d91e70c8bbdb6
Guilherme <guilhermeda2008@gmail.com>
2025-11-03 20:52:07 -0300

    (03-11-25)

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/FuncionarioController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/FuncionarioController.java
@@ -1,10 +1,37 @@
 package com.example.crudSpring.projetoCRUD.CONTROLLER;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.crudSpring.projetoCRUD.ENTITY.Funcionario;
+import com.example.crudSpring.projetoCRUD.SERVICE.EmpresaService;
+import com.example.crudSpring.projetoCRUD.SERVICE.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarioCTR") // localhost:8080/funcionario
 public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService ligacaoFuncionarioService;
+    @Autowired
+    private EmpresaService ligacaoEmpresaService;
+
+    @GetMapping("/listarFunc")
+    public String listarTodosFuncionario(Model oModel) {
+        oModel.addAttribute("funcionarios", ligacaoFuncionarioService.listarTodosFuncionarios());
+
+        return "listarFuncionarios"; // nome do html
+    }
+
+    @GetMapping("/formFuncionario")
+    public String mostrarFormCadastro(Model oModel) {
+
+        oModel.addAttribute("funcionario", new Funcionario());
+        oModel.addAttribute("empresas", ligacaoEmpresaService.findAll());
+        return "cadastrarFuncionario"; // nome do html
+    }
 
 }
